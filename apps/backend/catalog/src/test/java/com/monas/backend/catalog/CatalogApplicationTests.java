eab3ccf2cda10cb42ea4f8618cb5de59cd4fd1a7
fikretcansel <fikretcansel25@gmail.com>
2026-07-26 20:18:29 +0300

    create spring boot backend project

---
+++ apps/backend/catalog/src/test/java/com/monas/backend/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monas.backend.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
